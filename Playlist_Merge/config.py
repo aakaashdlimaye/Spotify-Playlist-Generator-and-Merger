a0c5a0c86b4aeb952dd3a4cc42aefcafe4edc016
aakaashdlimaye <aakaashdlimaye@gmail.com>
2026-02-11 00:28:07 +0530

    Update config.py

--- Playlist_Merge/config.py
+++ Playlist_Merge/config.py
@@ -1,5 +1,6 @@
-USERNAME = "7kc2wwfmntq91nsp1lj8kwp2m"
-CLIENT_ID = 'd290f18047ae4eab8524e6de53039b29'
-CLIENT_SECRET = 'b54e41f755504fe4a6e54407c51cffd7'
+USERNAME = ""
+CLIENT_ID = ''
+CLIENT_SECRET = ''
 REDIRECT_URI = 'http://localhost:8080/callback'
+
 SCOPE = 'playlist-modify-public'
